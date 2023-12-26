690a075d9c94228209aeb98d5fb70977b5caf063
EVERTONROGERIONUNES <70280660+EVERTONROGERIONUNES@users.noreply.github.com>
2023-12-26 13:54:18 -0300

    create search

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,13 +6,24 @@ use Illuminate\Http\Request;
 
 use App\Models\Event;
 
-
 class EventController extends Controller
 {
+
     public function index()
     {
-        $events = Event::all();
-        return view('welcome', ['events' => $events]);
+
+        $search = request('search');
+
+        if ($search) {
+
+            $events = Event::where([
+                ['title', 'like', '%' . $search . '%']
+            ])->get();
+        } else {
+            $events = Event::all();
+        }
+
+        return view('welcome', ['events' => $events, 'search' => $search]);
     }
 
     public function create()
@@ -26,9 +37,11 @@ class EventController extends Controller
         $event = new Event;
 
         $event->title = $request->title;
+        $event->date = $request->date;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
         // Image Upload
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
@@ -48,7 +61,7 @@ class EventController extends Controller
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
-    
+
     public function show($id)
     {
 
@@ -56,5 +69,4 @@ class EventController extends Controller
 
         return view('events.show', ['event' => $event]);
     }
-    
 }
