912d6c35ebb184a27d8554e73d8ee2abdeaa5bd9
EVERTONROGERIONUNES <70280660+EVERTONROGERIONUNES@users.noreply.github.com>
2024-01-10 23:03:09 -0300

    finalized project

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -59,7 +59,7 @@ class EventController extends Controller
         }
 
         $user = auth()->user();
-        $event->user_id =  $user->id;
+        $event->user_id = $user->id;
 
         $event->save();
 
@@ -71,9 +71,23 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if ($user) {
+
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach ($userEvents as $userEvent) {
+                if ($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
     }
 
     public function dashboard()
@@ -83,21 +97,33 @@ class EventController extends Controller
 
         $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view(
+            'events.dashboard',
+            ['events' => $events, 'eventsasparticipant' => $eventsAsParticipant]
+        );
     }
 
     public function destroy($id)
     {
 
         Event::findOrFail($id)->delete();
-
+        
         return redirect('/dashboard')->with('msg', 'Evento excluído com sucesso!');
+
     }
 
     public function edit($id)
     {
 
+        $user = auth()->user();
+
         $event = Event::findOrFail($id);
+
+        if ($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
 
         return view('events.edit', ['event' => $event]);
     }
@@ -124,5 +150,29 @@ class EventController extends Controller
         Event::findOrFail($request->id)->update($data);
 
         return redirect('/dashboard')->with('msg', 'Evento editado com sucesso!');
+    }
+
+    public function joinEvent($id)
+    {
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->attach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+    }
+
+    public function leaveEvent($id)
+    {
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->detach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento: ' . $event->title);
     }
 }
